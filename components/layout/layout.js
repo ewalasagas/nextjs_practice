0f828fc08e3bcc0d231787d62429a58a9f53f2bb
Elaine Alasagas <ealasagas@gmail.com>
2023-02-22 00:49:04 -0800

    adding components

---
+++ components/layout/layout.js
@@ -0,0 +1,15 @@
+const { Fragment } = require("react");
+const { default: MainHeader } = require("./main-header");
+
+function Layout(props) {
+    return (
+    <Fragment>
+        <MainHeader />
+        <main>
+            {props.children}
+        </main>
+    </Fragment>
+    )
+}
+
+export default Layout;
